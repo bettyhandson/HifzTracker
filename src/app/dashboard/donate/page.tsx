c93d8e026db30bfa4d0db96d52e68cacccb07b46
oprimecloud <abdulmaliksolagbade@gmail.com>
2026-02-03 20:48:44 +0000

    second depolyment

--- src/app/dashboard/donate/page.tsx
+++ src/app/dashboard/donate/page.tsx
@@ -30,7 +30,7 @@ export default function Donate({ userId }: { userId?: string }) {
 
   const handlePaystack = (amount: number) => {
     if (!amount || amount <= 0) {
-      alert("Please enter a valid amount to continue.");
+      alert("Please enter a valid amount t continue.");
       return;
     }
 
